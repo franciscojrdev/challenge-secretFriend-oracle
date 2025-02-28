526a7e350588e19f77288af24254fb44f53cc6b3
franciscojrdev <franciscojunior7222@gmail.com>
2025-02-27 22:47:41 -0300

    feat: listar o amigo secreto

--- app.js
+++ app.js
@@ -1,7 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
 let amigos = [];
-let escolherAmigo = gerarNumeroAleatorio();
-let limite = listaDeAmigos.length;
 
 function adicionarAmigo() {
   let nomeDoAmigo = document.getElementById("amigo").value;
@@ -10,15 +8,37 @@ function adicionarAmigo() {
   }
   amigos.push(nomeDoAmigo);
   criarLista(nomeDoAmigo);
+  esconderLista("resultado");
   limparCampo();
+}
+
+function sortearAmigo() {
+  let numero = gerarNumeroAleatorio();
+  if (amigos.length == 0 || numero == undefined) {
+    alert("Nenhum amigo adicionado a lista.");
+  } else {
+    let amigoSecreto = amigos[numero - 1];
+    mostrarAmigo(amigoSecreto);
+  }
 }
 
 function criarLista(nome) {
   let lista = document.getElementById("listaAmigos");
-  console.log(lista);
   let newLi = document.createElement("li");
   newLi.textContent = nome;
   lista.appendChild(newLi);
+}
+
+function mostrarAmigo(nome) {
+  let resultado = document.getElementById("resultado");
+  esconderLista("listaAmigos");
+  console.log(resultado);
+  resultado.innerHTML = "O seu amigo secreto é: " + nome;
+}
+
+function esconderLista(valor) {
+  let lista = document.getElementById(valor);
+  lista.innerHTML = "";
 }
 
 function limparCampo() {
@@ -27,5 +47,6 @@ function limparCampo() {
 }
 
 function gerarNumeroAleatorio() {
-  let numero = parseInt(Math.random() * limite + 1);
+  let numero = Math.floor(Math.random() * amigos.length + 1);
+  return numero;
 }
